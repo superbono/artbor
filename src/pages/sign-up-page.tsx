0cbba5a2b288cf0974af30cbae46bc20224e203c
superbono <fr.dev.ar@gmail.com>
2025-11-03 23:06:27 +0900

    회원가입 - 회원가입 UI 작업

--- src/pages/sign-up-page.tsx
+++ src/pages/sign-up-page.tsx
@@ -1,7 +1,58 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState, type SetStateAction } from "react";
+import { Link } from "react-router";
+
 export function SignUpPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleOnChangeEmail = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setEmail(e.target.value);
+  };
+  const handleOnChangePassword = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSignUpSubmit = () => {
+    // alert("회원가입");
+    if (email.trim() === "") return;
+    if (password.trim() === "") return;
+  };
+
   return (
-    <div>
-      <div>회원가입</div>
+    <div className="flex flex-col gap-8">
+      <div className="text-xl font-bold">회원가입</div>
+      <div className="flex flex-col gap-2">
+        <Input
+          className="py-6"
+          type="email"
+          placeholder="exam@exam.com"
+          value={email}
+          onChange={handleOnChangeEmail}
+        />
+        <Input
+          className="py-6"
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={handleOnChangePassword}
+        />
+      </div>
+      <div>
+        <Button className="w-full py-4" onClick={handleSignUpSubmit}>
+          회원가입
+        </Button>
+      </div>
+      <div>
+        <Link to={"/sign-in"} className="text-muted-foreground hover:underline">
+          이미 계정이 있다면 ? 로그인
+        </Link>
+      </div>
     </div>
   );
 }
