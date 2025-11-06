730ecde777bfffb21619c03c34a9f07dfcfd457e
superbono <fr.dev.ar@gmail.com>
2025-11-06 12:44:32 +0900

    로그인여부 - 현재 로그인이 되어있거나 비동기가 요청되기전에 권한 페이지가 노출이 되거나 비노출이 되는 현상을 방지하기 위한 코드작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import RootRoute from "@/routes/root-route";
+import supabase from "./lib/supabase";
+import { useEffect } from "react";
+import { useIsSessionLoaded, useSetSession } from "./store/session";
 
 export default function App() {
+  const setSession = useSetSession();
+  const isSessionLoaded = useIsSessionLoaded();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  if (!isSessionLoaded) return <div>Loading...</div>;
+
   return <RootRoute />;
 }
