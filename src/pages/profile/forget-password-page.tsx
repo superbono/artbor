9b01687ee1e51eb5d9889d3c0ee34eba61ac1ce1
superbono <fr.dev.ar@gmail.com>
2025-11-10 21:01:25 +0900

    비밀번호 재설정 - 비밀번호 변경을 위한 이메일 인증 페이지 / 비밀번호 재설정 페이지 UI 작업

--- src/pages/profile/forget-password-page.tsx
+++ src/pages/profile/forget-password-page.tsx
@@ -1,7 +1,41 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
 export function ForgetPasswordPage() {
+  const [email, setEmail] = useState("");
+
+  const handleSendEmailClick = () => {
+    if (email.trim() === "") return;
+  };
+
   return (
-    <div>
-      <div>비밀번호찾기</div>
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-1">
+        <div className="text-xl font-bold">비밀번호 잊으셨나요?</div>
+        <div className="text-muted-foreground">
+          이메일로 비밀번호를 재설정 할 수 있는 링크를 보내드릴게요.
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <Input
+          className="py-6"
+          type="email"
+          placeholder="exam@exam.com"
+          value={email}
+          onChange={(e) => e.target.value}
+          // disabled={}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Button
+          className="w-full"
+          // onClick={}
+          // disabled={}
+        >
+          인증 메일 요청하기
+        </Button>
+      </div>
     </div>
   );
 }
--- src/pages/profile/reset-password-page.tsx
+++ src/pages/profile/reset-password-page.tsx
@@ -1,7 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export function ResetPasswordPage() {
   return (
-    <div>
-      <div>비밀번호초기화</div>
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-1">
+        <div className="text-xl font-bold">비밀번호 재설정</div>
+        <div className="text-muted-foreground">
+          새로운 비밀번호를 입력하세요.
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <Input
+          className="py-6"
+          type="password"
+          placeholder="password"
+          // value={}
+          // onChange={}
+          // disabled={}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Button
+          className="w-full"
+          // onClick={}
+          // disabled={}
+        >
+          비밀번호 변경하기
+        </Button>
+      </div>
     </div>
   );
 }
