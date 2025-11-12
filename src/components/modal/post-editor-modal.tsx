08b047038d8b50ae96e10f80189a4e71c59713df
superbono <fr.dev.ar@gmail.com>
2025-11-12 19:45:44 +0900

    메인 - 모달 UI 작업

--- src/components/modal/post-editor-modal.tsx
+++ src/components/modal/post-editor-modal.tsx
@@ -12,14 +12,17 @@ export default function PostEditorModal() {
 
   return (
     <Dialog open={isOpen} onOpenChange={handleCloseModal}>
-      <DialogContent>
+      <DialogContent className="max-h-[190vh]">
         <DialogTitle>포스트 작성</DialogTitle>
-        <textarea />
-        <Button>
+        <textarea
+          className="max-h-125 min-h-25 focus:outline-none"
+          placeholder="무슨 일이 있었나요?"
+        />
+        <Button variant={"outline"} className="cursor-pointer">
           <ImageIcon />
           이미지추가
         </Button>
-        <Button>저장</Button>
+        <Button className="cursor-pointer">저장</Button>
       </DialogContent>
     </Dialog>
   );
