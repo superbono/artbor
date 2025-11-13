de1ba1f3676e23961d67f9a89704215a0148156f
superbono <fr.dev.ar@gmail.com>
2025-11-13 23:53:27 +0900

    메인 - 새로운 포스트 작성 모달에서 imge 첨부 후 저장시에 필요한 백엔드 로직 작업

--- src/components/modal/post-editor-modal copy.tsx
+++ src/components/modal/post-editor-modal copy.tsx
@@ -1,12 +1,16 @@
-import { ImageIcon, XIcon } from "lucide-react";
-import { Button } from "../ui/button";
-import { Dialog, DialogContent, DialogTitle } from "../ui/dialog";
-import { usePostEditorModal } from "@/store/post-editor-modal";
-import { useEffect, useRef, useState, type ChangeEvent } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { useCreatePost } from "@/hooks/mutations/post/use-create-post";
-import { toast } from "sonner";
-import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
+import { usePostEditorModal } from "@/store/post-editor-modal";
 import { useSession } from "@/store/session";
+import { ImageIcon, XIcon } from "lucide-react";
+import { useEffect, useRef, useState, type ChangeEvent } from "react";
+import { toast } from "sonner";
 
 type Image = {
   file: File;
@@ -16,6 +20,7 @@ type Image = {
 export default function PostEditorModal() {
   const session = useSession();
   const { isOpen, close } = usePostEditorModal();
+
   const { mutate: createPost, isPending: isCreatePostPending } = useCreatePost({
     onSuccess: () => {
       close();
@@ -26,6 +31,7 @@ export default function PostEditorModal() {
       });
     },
   });
+
   const [content, setContent] = useState("");
   const [images, setImages] = useState<Image[]>([]);
 
@@ -38,6 +44,7 @@ export default function PostEditorModal() {
 
   const handleCreatePostClick = () => {
     if (content.trim() === "") return;
+    console.log(images);
     createPost({
       content,
       images: images.map((image) => image.file),
@@ -81,23 +88,23 @@ export default function PostEditorModal() {
 
   return (
     <Dialog open={isOpen} onOpenChange={handleCloseModal}>
-      <DialogContent className="max-h-[190vh]">
+      <DialogContent className="max-h-[90vh]">
         <DialogTitle>포스트 작성</DialogTitle>
         <textarea
+          disabled={isCreatePostPending}
           ref={textareaRef}
           value={content}
           onChange={(e) => setContent(e.target.value)}
           className="max-h-125 min-h-25 focus:outline-none"
           placeholder="무슨 일이 있었나요?"
-          disabled={isCreatePostPending}
         />
         <input
+          onChange={handleSelectImages}
           ref={fileInputRef}
           type="file"
           accept="image/*"
           multiple
           className="hidden"
-          onChange={handleSelectImages}
         />
         {images.length > 0 && (
           <Carousel>
@@ -106,12 +113,12 @@ export default function PostEditorModal() {
                 <CarouselItem className="basis-2/5" key={image.previewUrl}>
                   <div className="relative">
                     <img
-                      className="h-full w-full rounded-sm object-cover"
                       src={image.previewUrl}
+                      className="h-full w-full rounded-sm object-cover"
                     />
                     <div
-                      className="absolute top-0 right-0 m-1 cursor-pointer rounded-full bg-black/20 p-1"
                       onClick={() => handleDeleteImage(image)}
+                      className="absolute top-0 right-0 m-1 cursor-pointer rounded-full bg-black/30 p-1"
                     >
                       <XIcon className="h-4 w-4 text-white" />
                     </div>
@@ -122,15 +129,15 @@ export default function PostEditorModal() {
           </Carousel>
         )}
         <Button
-          disabled={isCreatePostPending}
-          variant={"outline"}
-          className="cursor-pointer"
           onClick={() => {
             fileInputRef.current?.click();
           }}
+          disabled={isCreatePostPending}
+          variant={"outline"}
+          className="cursor-pointer"
         >
           <ImageIcon />
-          이미지추가
+          이미지 추가
         </Button>
         <Button
           disabled={isCreatePostPending}
